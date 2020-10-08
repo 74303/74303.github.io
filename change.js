501719e77bb62951b03c7eab2ebc938692e71f31
74303 <52731081+74303@users.noreply.github.com>
2020-10-08 19:48:52 +0900

    Add files via upload

---
+++ change.js
@@ -0,0 +1,39 @@
+function OnLinkClick(num){
+  switch(num){
+      /*case 0:
+        document.getElementById('home').style.display="block";
+        document.getElementById('about').style.display="none";
+	    document.getElementById('project').style.display="none";
+        document.getElementById('research').style.display="none";
+        break;
+        
+      case 1:
+        document.getElementById('home').style.display="none";
+        document.getElementById('about').style.display="block";
+	    document.getElementById('project').style.display="none";
+        document.getElementById('research').style.display="none";
+        break;
+        
+      case 2:
+        document.getElementById('home').style.display="none";
+        document.getElementById('about').style.display="none";
+	    document.getElementById('project').style.display="block";
+        document.getElementById('research').style.display="none";
+        break;
+        
+      case 3:
+        document.getElementById('home').style.display="none";
+        document.getElementById('about').style.display="none";
+	    document.getElementById('project').style.display="none";
+        document.getElementById('research').style.display="block";
+        break;*/
+      
+      case 10:
+        $('#kurione_movie1').prepend('<iframe class="embed-responsive-item" src="img/project/mixed-and-analysis.mp4"></iframe>');
+        break;
+        
+      case 11:
+        $('#othello_movie').prepend('<iframe class="embed-responsive-item" src="img/project/java_movie.mp4"></iframe>');
+        break;
+  }
+}
